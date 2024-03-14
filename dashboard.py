abefb0ee0b9fdd066b7d46f32f4c80320086272e
Anh Dunz <nad1362003@gmail.com>
2024-03-14 09:35:17 +0700

    giao diện chính

--- dashboard.py
+++ dashboard.py
@@ -7,8 +7,10 @@ class IMS:
         self.root.geometry("1350x700+0+0")
         self.root.title("- - - HỆ THỐNG QUẢN LÝ HIỆU SÁCH - - - NHÓM 9 - - - K15DCPM06 - - -")
         self.create_title_label()
-        self.create_clock_label()  # Gọi hàm tạo label clock
-        self.create_left_menu()     # Gọi hàm tạo left menu
+        self.create_clock_label()# Gọi hàm tạo label clock
+        self.create_footer_label()# Gọi hàm tạo footer
+        self.create_left_menu()# Gọi hàm tạo left menu
+        self.create_content_label()  # Gọi hàm tạo nội dung
         self.root.config(bg="#FFFFEC")
     
     #====title====
@@ -48,7 +50,7 @@ class IMS:
         btn_supplier = Button(self.leftMenu, text="Vật tư",image=self.icon_side,compound=LEFT,padx=10,anchor="w", font=("time new roman", 20, "bold"), bg="#CC9B6D", bd=3, cursor="hand2")
         btn_supplier.pack(side=TOP, fill=X)
 
-        btn_category = Button(self.leftMenu, text="danh mục",image=self.icon_side,compound=LEFT,padx=10,anchor="w", font=("time new roman", 20, "bold"), bg="#CC9B6D", bd=3, cursor="hand2")
+        btn_category = Button(self.leftMenu, text="Danh mục",image=self.icon_side,compound=LEFT,padx=10,anchor="w", font=("time new roman", 20, "bold"), bg="#CC9B6D", bd=3, cursor="hand2")
         btn_category.pack(side=TOP, fill=X)
         
         btn_product = Button(self.leftMenu, text="Sách",image=self.icon_side,compound=LEFT,padx=10,anchor="w", font=("time new roman", 20, "bold"), bg="#CC9B6D", bd=3, cursor="hand2")
@@ -56,6 +58,29 @@ class IMS:
         
         btn_sales = Button(self.leftMenu, text="Lợi tức",image=self.icon_side,compound=LEFT,padx=10,anchor="w", font=("time new roman", 20, "bold"), bg="#CC9B6D", bd=3, cursor="hand2")
         btn_sales.pack(side=TOP, fill=X)
+
+    #========content=========
+    def create_content_label(self):
+        self.lbl_employee = Label(self.root, text="Tổng số nhân viên\n[ 0 ]",bd=5,relief=RIDGE, bg="#5F6F52", fg="#FFF4F4", font=("Arial Rounded MT Bold", 20, "bold"))
+        self.lbl_employee.place(x=300, y=120, height=150, width=300)
+
+        self.lbl_supplier = Label(self.root, text="Tổng số vật tư\n[ 0 ]",bd=5,relief=RIDGE, bg="#898121", fg="#FFF4F4", font=("Arial Rounded MT Bold", 20, "bold"))
+        self.lbl_supplier.place(x=650, y=120, height=150, width=300)
+
+        self.lbl_category = Label(self.root, text="Tổng số danh mục\n[ 0 ]",bd=5,relief=RIDGE, bg="#ABC270", fg="#FFF4F4", font=("Arial Rounded MT Bold", 20, "bold"))
+        self.lbl_category.place(x=1000, y=120, height=150, width=300)
+
+        self.lbl_product = Label(self.root, text="Tổng số sách\n[ 0 ]",bd=5,relief=RIDGE, bg="#94B49F", fg="#FFF4F4", font=("Arial Rounded MT Bold", 20, "bold"))
+        self.lbl_product.place(x=300, y=300, height=150, width=300)
+
+        self.lbl_sales = Label(self.root, text="Tổng doanh thu\n[ 0 ]",bd=5,relief=RIDGE, bg="#95CD41", fg="#FFF4F4", font=("Arial Rounded MT Bold", 20, "bold"))
+        self.lbl_sales.place(x=650, y=300, height=150, width=300)
+
+    #====footer====
+    def create_footer_label(self):
+        self.lbl_footer = Label(self.root, text="Hệ thống quản lý hiệu sách  |  phát triển bởi nhóm 9 -lớp K15DCPM06\nNguyễn Anh Dũng  -  Nguyễn Trọng Nghĩa  -  Nguyễn Hoàng Giang", font=("Arial Rounded MT Bold", 12, "bold"), bg="#76453B", fg="#F8FAE5").pack(side=BOTTOM,fill=X) 
+
+
 
 root = Tk()
 obj = IMS(root)
