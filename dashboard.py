a8cb6ac8c0c7e29f0dd65522e7b26e682c11d272
Anh Dunz <nad1362003@gmail.com>
2024-03-14 19:38:08 +0700

    smaller :(( less option :((

--- dashboard.py
+++ dashboard.py
@@ -1,92 +1,72 @@
 from tkinter import *
-from PIL import Image, ImageTk #pip install pillow
+from PIL import Image, ImageTk  # pip install pillow
 from employee import employeeClass
 class IMS:
     def __init__(self, root):
         self.root = root
-        self.root.geometry("1350x700+0+0")
+        self.root.geometry("1000x567+0+0")
         self.root.title("- - - HỆ THỐNG QUẢN LÝ HIỆU SÁCH - - - NHÓM 9 - - - K15DCPM06 - - -")
         self.create_title_label()
-        self.create_clock_label()# Gọi hàm tạo label clock
-        self.create_footer_label()# Gọi hàm tạo footer
-        self.create_left_menu()# Gọi hàm tạo left menu
+        self.create_clock_label()  # Gọi hàm tạo label clock
+        self.create_footer_label()  # Gọi hàm tạo footer
+        self.create_left_menu()  # Gọi hàm tạo left menu
         self.create_content_label()  # Gọi hàm tạo nội dung
         self.root.config(bg="#FFFFEC")
-    
-    #====title====
+    # ====title====
     def create_title_label(self):
         self.icon_title = PhotoImage(file="images/book.png")
-        title = Label(self.root, text="HỆ THỐNG QUẢN LÝ HIỆU SÁCH", image=self.icon_title, compound=LEFT , font=("Arial Rounded MT Bold", 30, "bold"), bg="#B19470", fg="#F8FAE5", anchor="w", padx=20)
+        title = Label(self.root, text="HỆ THỐNG QUẢN LÝ HIỆU SÁCH", image=self.icon_title, compound=LEFT, font=("time new roman", 30, "bold"), bg="#B19470", fg="#F8FAE5", anchor="w", padx=20)
         title.place(x=0, y=0, relwidth=1, height=70)
-
-        #====btn_logout====
+    # ====btn_logout====
         btn_logout = Button(self.root, text="Thoát", font=("time new roman", 15, "bold"), bg="#BECA5C")
-        btn_logout.place(x=1230, y=10, width=100)
-
-    #====clock====
+        btn_logout.place(x=885, y=14, width=100)
+    # ====clock====
     def create_clock_label(self):
-        self.lbl_clock = Label(self.root, text="Chào mừng đã đến với hệ thống\t\t Date: DD-MM-YYYY\t\t Time: HH:MM:SS", font=("Arial Rounded MT Bold", 15), bg="#E1C78F", fg="#F8FAE5")
-        self.lbl_clock.place(x=0, y=70, relwidth=1, height=30)  
-
-    #====Left menu====
+        self.lbl_clock = Label(self.root, text="Chào mừng đã đến với hệ thống\t Date: DD-MM-YYYY\t Time: HH:MM:SS",font=("time new roman", 15), bg="#E1C78F", fg="#F8FAE5")
+        self.lbl_clock.place(x=0, y=70, relwidth=1, height=30)
+    # ====Left menu====
     def create_left_menu(self):
         self.MenuLogo = Image.open("images/menu.png")
         self.MenuLogo = self.MenuLogo.resize((200, 200), Image.LANCZOS)
         self.MenuLogo = ImageTk.PhotoImage(self.MenuLogo)
 
         self.leftMenu = Frame(self.root, bd=2, relief=RIDGE, bg="#F3EEEA")
-        self.leftMenu.place(x=0, y=102, width=200, height=533)
-        
+        self.leftMenu.place(x=0, y=102, width=205, height=420)
+
         lbl_menuLogo = Label(self.leftMenu, image=self.MenuLogo)
         lbl_menuLogo.pack(side=TOP, fill=X)
 
-        lbl_menu = Label(self.leftMenu, text="Menu", font=("time new roman", 25, "bold"), bg="#76453B",fg="#F8FAE5")
+        lbl_menu = Label(self.leftMenu, text="Menu", font=("time new roman", 25, "bold"), bg="#76453B", fg="#F8FAE5")
         lbl_menu.pack(side=TOP, fill=X)
-        
-        self.icon_side=PhotoImage(file="images/side.png")
-        btn_employee = Button(self.leftMenu, text="Nhân viên",command=self.employee,image=self.icon_side,compound=LEFT,padx=10,anchor="w", font=("time new roman", 20, "bold"), bg="#CC9B6D", bd=3, cursor="hand2")
+
+        self.icon_side = PhotoImage(file="images/side.png")
+        btn_employee = Button(self.leftMenu, text="Nhân viên", command=self.employee, image=self.icon_side,compound=LEFT, padx=10, anchor="w", font=("time new roman", 20, "bold"), bg="#CC9B6D", bd=3, cursor="hand2")
         btn_employee.pack(side=TOP, fill=X)
 
-        btn_supplier = Button(self.leftMenu, text="Vật tư",image=self.icon_side,compound=LEFT,padx=10,anchor="w", font=("time new roman", 20, "bold"), bg="#CC9B6D", bd=3, cursor="hand2")
-        btn_supplier.pack(side=TOP, fill=X)
-
-        btn_category = Button(self.leftMenu, text="Danh mục",image=self.icon_side,compound=LEFT,padx=10,anchor="w", font=("time new roman", 20, "bold"), bg="#CC9B6D", bd=3, cursor="hand2")
+        btn_category = Button(self.leftMenu, text="Danh mục", image=self.icon_side, compound=LEFT, padx=10, anchor="w", font=("time new roman", 20, "bold"), bg="#CC9B6D", bd=3, cursor="hand2")
         btn_category.pack(side=TOP, fill=X)
-        
-        btn_product = Button(self.leftMenu, text="Sách",image=self.icon_side,compound=LEFT,padx=10,anchor="w", font=("time new roman", 20, "bold"), bg="#CC9B6D", bd=3, cursor="hand2")
+
+        btn_product = Button(self.leftMenu, text="Sách", image=self.icon_side, compound=LEFT, padx=10,anchor="w", font=("time new roman", 20, "bold"), bg="#CC9B6D", bd=3, cursor="hand2")
         btn_product.pack(side=TOP, fill=X)
-        
-        btn_sales = Button(self.leftMenu, text="Lợi tức",image=self.icon_side,compound=LEFT,padx=10,anchor="w", font=("time new roman", 20, "bold"), bg="#CC9B6D", bd=3, cursor="hand2")
-        btn_sales.pack(side=TOP, fill=X)
-
-    #========content=========
+    # ========content=========
     def create_content_label(self):
-        self.lbl_employee = Label(self.root, text="Tổng số nhân viên\n[ 0 ]",bd=5,relief=RIDGE, bg="#5F6F52", fg="#FFF4F4", font=("Arial Rounded MT Bold", 20, "bold"))
-        self.lbl_employee.place(x=300, y=120, height=150, width=300)
+        self.lbl_employee = Label(self.root, text="Tổng số nhân viên\n[ 0 ]", bd=5, relief=RIDGE, bg="#5F6F52", fg="#FFF4F4", font=("time new roman", 20, "bold"))
+        self.lbl_employee.place(x=290, y=120, height=150, width=300)
 
-        self.lbl_supplier = Label(self.root, text="Tổng số vật tư\n[ 0 ]",bd=5,relief=RIDGE, bg="#898121", fg="#FFF4F4", font=("Arial Rounded MT Bold", 20, "bold"))
-        self.lbl_supplier.place(x=650, y=120, height=150, width=300)
+        self.lbl_category = Label(self.root, text="Tổng số danh mục\n[ 0 ]", bd=5, relief=RIDGE, bg="#ABC270",fg="#FFF4F4", font=("time new roman", 20, "bold"))
+        self.lbl_category.place(x=650, y=120, height=150, width=300)
 
-        self.lbl_category = Label(self.root, text="Tổng số danh mục\n[ 0 ]",bd=5,relief=RIDGE, bg="#ABC270", fg="#FFF4F4", font=("Arial Rounded MT Bold", 20, "bold"))
-        self.lbl_category.place(x=1000, y=120, height=150, width=300)
-
-        self.lbl_product = Label(self.root, text="Tổng số sách\n[ 0 ]",bd=5,relief=RIDGE, bg="#94B49F", fg="#FFF4F4", font=("Arial Rounded MT Bold", 20, "bold"))
-        self.lbl_product.place(x=300, y=300, height=150, width=300)
-
-        self.lbl_sales = Label(self.root, text="Tổng doanh thu\n[ 0 ]",bd=5,relief=RIDGE, bg="#95CD41", fg="#FFF4F4", font=("Arial Rounded MT Bold", 20, "bold"))
-        self.lbl_sales.place(x=650, y=300, height=150, width=300)
-
-    #====footer====
+        self.lbl_product = Label(self.root, text="Tổng số sách\n[ 0 ]", bd=5, relief=RIDGE, bg="#94B49F", fg="#FFF4F4", font=("time new roman", 20, "bold"))
+        self.lbl_product.place(x=290, y=300, height=150, width=300)
+    # ====footer====
     def create_footer_label(self):
-        self.lbl_footer = Label(self.root, text="Hệ thống quản lý hiệu sách  |  phát triển bởi nhóm 9 -lớp K15DCPM06\nNguyễn Anh Dũng  -  Nguyễn Trọng Nghĩa  -  Nguyễn Hoàng Giang", font=("Arial Rounded MT Bold", 12, "bold"), bg="#76453B", fg="#F8FAE5").pack(side=BOTTOM,fill=X) 
-
-#===============================================================================================================================================================================================
-
+        self.lbl_footer = Label(self.root,text="Hệ thống quản lý hiệu sách  |  phát triển bởi nhóm 9 -lớp K15DCPM06\nNguyễn Anh Dũng  -  Nguyễn Trọng Nghĩa  -  Nguyễn Hoàng Giang", font=("time new roman", 12, "bold"), bg="#76453B", fg="#F8FAE5").pack(side=BOTTOM,fill=X)
+    # ===============================================================================================================================================================================================
     def employee(self):
-        self.new_win=Toplevel(self.root)
-        self.obj=employeeClass(self.new_win)
+        self.new_win = Toplevel(self.root)
+        self.obj = employeeClass(self.new_win)
 
-if __name__=="__main__":
+if __name__ == "__main__":
     root = Tk()
     obj = IMS(root)
     root.mainloop()
--- employee.py
+++ employee.py
@@ -5,7 +5,7 @@ from tkinter import ttk
 class employeeClass:
     def __init__(self, root):
         self.root = root
-        self.root.geometry("1100x500+220+130")
+        self.root.geometry("792x390+206+130")
         self.root.title("- - - HỆ THỐNG QUẢN LÝ HIỆU SÁCH - - - NHÓM 9 - - - K15DCPM06 - - -")
         self.root.config(bg="#FFFFEC")
         self.root.focus_force()
@@ -26,25 +26,24 @@ class employeeClass:
 
         #===== khung tìm kiếm ===========
         SearchFrame=LabelFrame(self.root,text="Tìm nhân viên",font=("time new roman",12,"bold"),bd=2,relief=RIDGE,bg="#F1E4C3")
-        SearchFrame.place(x=250,y=20,width=600,height=70)
+        SearchFrame.place(x=150,y=20,width=500,height=70)
         
         #===== các chức năng =========
         cmb_search = ttk.Combobox(SearchFrame, textvariable=self.var_searchtxt, values=("Chọn theo","Tên", "Email", "Contact"), state='readonly', justify=CENTER, font=("time new roman", 15))
-        cmb_search.place(x=10, y=10, width=180)
+        cmb_search.place(x=10, y=10, width=130)
         cmb_search.current(0)
-        txt_search = Entry(SearchFrame, font=("time new roman", 15), bg="#F3EEEA").place(x=200, y=10)
-        btn_search = Button(SearchFrame, text="Tìm", font=("Arial Rounded MT Bold", 17), bg="#C85108", fg="#F4EEC7").place(x=470, y=8, width=80, height=30)
-
+        txt_search = Entry(SearchFrame, font=("time new roman", 15), bg="#F3EEEA").place(x=160, y=10)
+        btn_search = Button(SearchFrame, text="Tìm", font=("Arial Rounded MT Bold", 17), bg="#C85108", fg="#F4EEC7").place(x=400, y=8, width=80, height=30)
 
     #==============title======
-        title = Label(self.root, text="thông tin chi tiết của nhân viên", font=("time new roman", 15), bg="#61764B", fg="#FFF9C9").place(x=50, y=100, width=1000)
+        title = Label(self.root, text="thông tin chi tiết của nhân viên", font=("time new roman", 15), bg="#61764B", fg="#FFF9C9").place(x=5, y=100, width=782)
 
     #============content=========
     #============= hàng 1 ==========
-        lbl_empid = Label(self.root, text="Mã số", font=("time new roman", 15), bg="#F1E4C3", fg="#000000").place(x=50, y=150)
-        lbl_gender = Label(self.root, text="Giới tính", font=("time new roman", 15), bg="#F1E4C3", fg="#000000").place(x=350, y=150)
-        lbl_contact = Label(self.root, text="Liên hệ", font=("time new roman", 15), bg="#F1E4C3", fg="#000000").place(x=750, y=150)
-        txt_empid = Entry(self.root, textvariable= self.var_emp_id, font=("time new roman", 15), bg="#F3EEEA", fg="#000000").place(x=122, y=150, width=209)
+        lbl_empid = Label(self.root, text="Mã số", font=("time new roman", 15), bg="#F1E4C3", fg="#000000").place(x=5, y=150)
+        lbl_gender = Label(self.root, text="Giới tính", font=("time new roman", 15), bg="#F1E4C3", fg="#000000").place(x=295, y=150)
+        lbl_contact = Label(self.root, text="Liên hệ", font=("time new roman", 15), bg="#F1E4C3", fg="#000000").place(x=705, y=150)
+        txt_empid = Entry(self.root, textvariable= self.var_emp_id, font=("time new roman", 15), bg="#F3EEEA", fg="#000000").place(x=70, y=150, width=209)
         cmb_gender = ttk.Combobox(self.root, textvariable=self.var_gender, values=("Chọn theo","Nam", "Nữ"), state='readonly', justify=CENTER, font=("time new roman", 15))
         cmb_gender.place(x=440, y=150, width=190)
         cmb_gender.current(0)
@@ -52,12 +51,12 @@ class employeeClass:
         txt_contact = Entry(self.root, textvariable=self.var_contact, font=("time new roman", 15), bg="#F3EEEA", fg="#000000")
         txt_contact.place(x=830, y=150, width=180)
     #======== hàng 2 ===========
-        lbl_name = Label(self.root, text="Họ và tên", font=("time new roman", 15), bg="#F1E4C3", fg="#000000").place(x=50, y=190)
+        lbl_name = Label(self.root, text="Họ và tên", font=("time new roman", 15), bg="#F1E4C3", fg="#000000").place(x=5, y=190)
         lbl_dob = Label(self.root, text="Ngày sinh", font=("time new roman", 15), bg="#F1E4C3", fg="#000000").place(x=350, y=190)        
         lbl_doj = Label(self.root, text="Ngày tham gia", font=("time new roman", 15), bg="#F1E4C3", fg="#000000").place(x=750, y=190)
         
         txt_name = Entry(self.root, textvariable=self.var_name, font=("time new roman", 15), bg="#F3EEEA", fg="#000000")
-        txt_name.place(x=150, y=190, width=180)
+        txt_name.place(x=98, y=190, width=180)
 
         txt_dob = Entry(self.root, textvariable=self.var_dob, font=("time new roman", 15), bg="#F3EEEA", fg="#000000")
         txt_dob.place(x=450, y=190, width=180)
@@ -66,12 +65,12 @@ class employeeClass:
         txt_doj.place(x=890, y=190, width=120)
 
 #======== hàng 3 ===========
-        lbl_email = Label(self.root, text="Email", font=("time new roman", 15), bg="#F1E4C3", fg="#000000").place(x=50, y=230)
+        lbl_email = Label(self.root, text="Email", font=("time new roman", 15), bg="#F1E4C3", fg="#000000").place(x=5, y=230)
         lbl_pass = Label(self.root, text="Mật khẩu", font=("time new roman", 15), bg="#F1E4C3", fg="#000000").place(x=350, y=230)        
         lbl_utype = Label(self.root, text="phân quyền", font=("time new roman", 15), bg="#F1E4C3", fg="#000000").place(x=750, y=230)
         
         txt_email = Entry(self.root, textvariable=self.var_email, font=("time new roman", 15), bg="#F3EEEA", fg="#000000")
-        txt_email.place(x=115, y=230, width=216)
+        txt_email.place(x=63, y=230, width=216)
 
         txt_pass = Entry(self.root, textvariable=self.var_pass, font=("time new roman", 15), bg="#F3EEEA", fg="#000000")
         txt_pass.place(x=450, y=230, width=180)
