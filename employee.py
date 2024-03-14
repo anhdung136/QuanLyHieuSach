5d64bfdc9da4d61e77315507d1c5238b5fb29857
Anh Dunz <nad1362003@gmail.com>
2024-03-14 16:17:48 +0700

    3/5 chức năng nhân viên

--- employee.py
+++ employee.py
@@ -24,30 +24,60 @@ class employeeClass:
         self.var_pass = StringVar()
         self.var_utype = StringVar()
 
-
-
         #===== khung tìm kiếm ===========
-        SearchFrame=LabelFrame(self.root,text="Tìm nhân viên",font=("time new roman",12,"bold"),bd=2,relief=RIDGE,bg="white")
+        SearchFrame=LabelFrame(self.root,text="Tìm nhân viên",font=("time new roman",12,"bold"),bd=2,relief=RIDGE,bg="#F1E4C3")
         SearchFrame.place(x=250,y=20,width=600,height=70)
         
         #===== các chức năng =========
-        cmb_search = ttk.Combobox(SearchFrame, textvariable=self.var_searchtxt, values=("Tên", "Email", "Contact"), state='readonly', justify=CENTER, font=("time new roman", 15))
+        cmb_search = ttk.Combobox(SearchFrame, textvariable=self.var_searchtxt, values=("Chọn theo","Tên", "Email", "Contact"), state='readonly', justify=CENTER, font=("time new roman", 15))
         cmb_search.place(x=10, y=10, width=180)
         cmb_search.current(0)
+        txt_search = Entry(SearchFrame, font=("time new roman", 15), bg="#F3EEEA").place(x=200, y=10)
+        btn_search = Button(SearchFrame, text="Tìm", font=("Arial Rounded MT Bold", 17), bg="#C85108", fg="#F4EEC7").place(x=470, y=8, width=80, height=30)
+
+
+    #==============title======
+        title = Label(self.root, text="thông tin chi tiết của nhân viên", font=("time new roman", 15), bg="#61764B", fg="#FFF9C9").place(x=50, y=100, width=1000)
+
+    #============content=========
+    #============= hàng 1 ==========
+        lbl_empid = Label(self.root, text="Mã số", font=("time new roman", 15), bg="#F1E4C3", fg="#000000").place(x=50, y=150)
+        lbl_gender = Label(self.root, text="Giới tính", font=("time new roman", 15), bg="#F1E4C3", fg="#000000").place(x=350, y=150)
+        lbl_contact = Label(self.root, text="Liên hệ", font=("time new roman", 15), bg="#F1E4C3", fg="#000000").place(x=750, y=150)
+        txt_empid = Entry(self.root, textvariable= self.var_emp_id, font=("time new roman", 15), bg="#F3EEEA", fg="#000000").place(x=122, y=150, width=209)
+        cmb_gender = ttk.Combobox(self.root, textvariable=self.var_gender, values=("Chọn theo","Nam", "Nữ"), state='readonly', justify=CENTER, font=("time new roman", 15))
+        cmb_gender.place(x=440, y=150, width=190)
+        cmb_gender.current(0)
+
+        txt_contact = Entry(self.root, textvariable=self.var_contact, font=("time new roman", 15), bg="#F3EEEA", fg="#000000")
+        txt_contact.place(x=830, y=150, width=180)
+    #======== hàng 2 ===========
+        lbl_name = Label(self.root, text="Họ và tên", font=("time new roman", 15), bg="#F1E4C3", fg="#000000").place(x=50, y=190)
+        lbl_dob = Label(self.root, text="Ngày sinh", font=("time new roman", 15), bg="#F1E4C3", fg="#000000").place(x=350, y=190)        
+        lbl_doj = Label(self.root, text="Ngày tham gia", font=("time new roman", 15), bg="#F1E4C3", fg="#000000").place(x=750, y=190)
         
-        txt_search = Entry(SearchFrame, font=("time new roman", 15), bg="#FEF7DC")
-        txt_search.place(x=200, y=10)
-        btn_search=Button(SearchFrame,text="Tìm", textvariable=self.var_searchtxt, font=("Arial Rounded MT Bold", 17), bg="#C85108",fg="#F4EEC7")
-        btn_search.place(x=470, y=8,width=80,height=30)
+        txt_name = Entry(self.root, textvariable=self.var_name, font=("time new roman", 15), bg="#F3EEEA", fg="#000000")
+        txt_name.place(x=150, y=190, width=180)
 
-        #========title======
-        title = Label(self.root, text="thông tin chi tiết của nhân viên", font=("time new roman", 15), bg="#61764B", fg="#FFF9C9")
-        title.place(x=50, y=100, width=1000)
+        txt_dob = Entry(self.root, textvariable=self.var_dob, font=("time new roman", 15), bg="#F3EEEA", fg="#000000")
+        txt_dob.place(x=450, y=190, width=180)
 
-        #======content=========
-        lbl_empid = Label(self.root, text="Mã số nhân viên", font=("time new roman", 15), bg="#61764B", fg="#FFF9C9")
-        lbl_empid.place(x=50, y=150)
+        txt_doj = Entry(self.root, textvariable=self.var_doj, font=("time new roman", 15), bg="#F3EEEA", fg="#000000")
+        txt_doj.place(x=890, y=190, width=120)
 
+#======== hàng 3 ===========
+        lbl_email = Label(self.root, text="Email", font=("time new roman", 15), bg="#F1E4C3", fg="#000000").place(x=50, y=230)
+        lbl_pass = Label(self.root, text="Mật khẩu", font=("time new roman", 15), bg="#F1E4C3", fg="#000000").place(x=350, y=230)        
+        lbl_utype = Label(self.root, text="phân quyền", font=("time new roman", 15), bg="#F1E4C3", fg="#000000").place(x=750, y=230)
+        
+        txt_email = Entry(self.root, textvariable=self.var_email, font=("time new roman", 15), bg="#F3EEEA", fg="#000000")
+        txt_email.place(x=115, y=230, width=216)
+
+        txt_pass = Entry(self.root, textvariable=self.var_pass, font=("time new roman", 15), bg="#F3EEEA", fg="#000000")
+        txt_pass.place(x=450, y=230, width=180)
+
+        txt_utype = Entry(self.root, textvariable=self.var_utype, font=("time new roman", 15), bg="#F3EEEA", fg="#000000")
+        txt_utype.place(x=870, y=230, width=141)
 
 if __name__ == "__main__":
     root = Tk()
