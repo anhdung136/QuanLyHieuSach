955c7f933ec123e7aba6651c461932ef6000c270
Anh Dunz <nad1362003@gmail.com>
2024-03-28 09:16:22 +0700

    category

--- category.py
+++ category.py
@@ -10,8 +10,37 @@ class categoryClass:
         self.root.title("- 📔 - HỆ THỐNG QUẢN LÝ HIỆU SÁCH - 📚 - NHÓM 9 - 📖 - K15DCPM06 - 📝 -")
         self.root.config(bg="#FFFFEC")
         self.root.focus_force()
+        #------variables------
+        self.var_catid = StringVar()
+        self.var_name = StringVar()
+
     #============title===============
-        lbl_titel = Label(self.root, text="Quản Lý Danh Mục", font=("time new roman",30),bg="grey",fg="white").pack(side=TOP)
+        lbl_titel = Label(self.root, text="Quản Lý Danh Mục", font=("time new roman",30),bg="grey",fg="white",relief=RIDGE).pack(side=TOP,fill=X,padx=10,pady=2)
+
+        lbl_name = Label(self.root, text="Nhập Danh Mục", font=("time new roman",30),bg="grey").place(x=50,y=100)
+        lbl_name = Entry(self.root, textvariable= self.var_name, font=("time new roman",20),bg="lightyellow").place(x=50,y=170,width=290)
+
+        btn_add = Button(self.root, text="Thêm", font=("time new roman",18),bg="green",fg="black", cursor="hand2").place(x=50,y=230,width=100,height=30)
+        btn_delete = Button(self.root, text="Xóa", font=("time new roman",18),bg="red",fg="black", cursor="hand2").place(x=240,y=230,width=100,height=30)
+    #========--category details--============
+        cat_frame=Frame(self.root,bd=3,relief=RIDGE)
+        cat_frame.place(x=400,y=100,width=380,height=350)
+
+        scrolly=Scrollbar(cat_frame,orient=VERTICAL)
+        scrollx=Scrollbar(cat_frame,orient=HORIZONTAL)
+
+        self.category_table=ttk.Treeview(cat_frame,columns=("cid","name"),yscrollcommand=scrolly,xscrollcommand=scrollx)
+        scrollx.pack(side=BOTTOM,fill=X)
+        scrolly.pack(side=RIGHT,fill=Y)
+        scrollx.config(command=self.category_table.xview)
+        scrolly.config(command=self.category_table.yview)
+
+        self.category_table.heading("cid",text="Mã số danh mục")
+        self.category_table.heading("name",text="Tên danh mục")
+        self.category_table["show"]="headings"
+        self.category_table.column("cid",width=30)
+        self.category_table.column("name",width=100)
+        self.category_table.pack(fill=BOTH,expand=1)
 
 
 if __name__ == "__main__":
